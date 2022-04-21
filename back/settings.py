6af05d17e4bd9e9e138e73bc61749556fc2af8ad
soto <z@mcac.cc>
2022-04-21 22:25:18 +0800

    增加了原生管理员页面

--- back/settings.py
+++ back/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -123,9 +123,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from myapp import models
+
+admin.site.register(models.User)
+admin.site.register(models.Post)
+admin.site.register(models.Comment)
+admin.site.register(models.Pet)
+admin.site.register(models.Hospital)
+admin.site.register(models.Rating)
+admin.site.register(models.Star)
+admin.site.register(models.VetHospital)
+admin.site.register(models.WalkPoint)

27ec842512936da9cef9716c44bdde283eddb9b0
soto <z@mcac.cc>
2022-05-05 14:29:53 +0800

    修改部分views为CBV

--- myapp/views.py
+++ myapp/views.py
@@ -2,10 +2,8 @@ from datetime import timezone
 
 from django.contrib import auth
 from django.http import JsonResponse
-from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework import viewsets
 from rest_framework.views import APIView
 
 from myapp.models import *
@@ -13,8 +11,6 @@ import myapp.verification as verification
 import myapp.openIM as openIM
 import myapp.info as info
 import logging
-
-from myapp.serializers import UserSerializer
 
 logger = logging.getLogger(__name__)
 
@@ -54,106 +50,139 @@ class UserInfo(APIView):
         return JsonResponse({"success": True, "message": "用户信息设置成功"})
 
 
-@csrf_exempt
-def login(request):
-    if request.method == "GET":
-        return render(request, "login.html")
-    username = request.POST.get("userName")
-    password = request.POST.get("userPassword")
-    user = auth.authenticate(username=username, password=password)
-    if user is not None:
-        auth.login(request, user)
-        return JsonResponse({"success": True, "message": "登录成功"})
-    return JsonResponse({"success": False, "message": "用户名或密码错误"})
+class LoginView(APIView):
+    @swagger_auto_schema(
+        operation_summary='用户登录',
+        response={200: 'OK'}
+    )
+    def post(self, request):
+        username = request.POST.get("userName")
+        password = request.POST.get("userPassword")
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return JsonResponse({"success": True, "message": "登录成功"})
+        return JsonResponse({"success": False, "message": "用户名或密码错误"})
 
 
-@csrf_exempt
-def register(request):
-    if request.method == "GET":
-        return render(request, "register.html")
-    username = request.POST.get("userName")
-    password = request.POST.get("userPassword")
-    user = User.objects.create_user(username=username, password=password)
-    if user is not None:
-        user.is_active = False
-        token = openIM.register(user.username)
-        return JsonResponse({"success": True, "message": "注册成功", "im_token": token})
-    return JsonResponse({"success": False, "message": "用户名已存在"})
+class RegisterView(APIView):
+    @swagger_auto_schema(
+        operation_summary='用户注册',
+        response={200: 'OK'}
+    )
+    def post(self, request):
+        username = request.POST.get("userName")
+        password = request.POST.get("userPassword")
+        email = request.POST.get("userEmail")
+        user = User.objects.create_user(username=username, password=password, email=email)
+        if user is not None:
+            user.is_active = False
+            token = openIM.register(user.username)
+            return JsonResponse({"success": True, "message": "注册成功", "im_token": token})
+        return JsonResponse({"success": False, "message": "用户名已存在"})
 
 
-@csrf_exempt
-def logout(request):
-    auth.logout(request)
-    return JsonResponse({"success": True, "message": "注销成功"})
+class LogoutView(APIView):
+    @swagger_auto_schema(
+        operation_summary='用户注销',
+        response={200: 'OK'}
+    )
+    def get(self, request):
+        auth.logout(request)
+        return JsonResponse({"success": True, "message": "注销成功"})
 
 
-def set_pet_info(request):
-    if request.method == "GET":
-        return render(request, "set_pet_info.html")
-    new_pet = Pet()
-    new_pet.pet_name = request.POST.get("petName")
-    new_pet.pet_type = request.POST.get("petType")
-    new_pet.pet_breed = request.POST.get("petBreed")
-    new_pet.pet_gender = request.POST.get("petGender")
-    new_pet.pet_date_of_birth = request.POST.get("petDateOfBirth")
-    new_pet.save()
-    user_id = request.session.get('_auth_user_id')
-    user = User.objects.get(id=user_id)
-    user.pet = new_pet
-    user.save()
-    return JsonResponse({"success": True, "message": "宠物信息设置成功"})
+class PetView(APIView):
+    @swagger_auto_schema(
+        operation_summary='获取宠物信息',
+        response={200: 'OK'}
+    )
+    def get(self, request):
+        user_id = request.session.get('_auth_user_id')
+        user = User.objects.get(id=user_id)
+        pet = user.pet
+        pet_info = info.get_pet_info(pet)
+        return JsonResponse(pet_info)
+
+    @swagger_auto_schema(
+        operation_summary='修改宠物信息',
+        response={200: 'OK'}
+    )
+    def post(self, request):
+        new_pet = Pet()
+        new_pet.pet_name = request.POST.get("petName")
+        new_pet.pet_type = request.POST.get("petType")
+        new_pet.pet_breed = request.POST.get("petBreed")
+        new_pet.pet_gender = request.POST.get("petGender")
+        new_pet.pet_date_of_birth = request.POST.get("petDateOfBirth")
+        new_pet.save()
+        user_id = request.session.get('_auth_user_id')
+        user = User.objects.get(id=user_id)
+        user.pet = new_pet
+        user.save()
+        return JsonResponse({"success": True, "message": "宠物信息设置成功"})
 
 
-def get_pet_info(request):
-    user_id = request.session.get('_auth_user_id')
-    user = User.objects.get(id=user_id)
-    pet = user.pet
-    pet_info = info.get_pet_info(pet)
-    return JsonResponse(pet_info)
+class VerificationCodeView(APIView):
+    @swagger_auto_schema(
+        operation_summary='获取验证码',
+        response={200: 'OK'}
+    )
+    def get(self, request):
+        user_id = request.session.get('_auth_user_id')
+        user = User.objects.get(id=user_id)
+        verification.send_code(user.email)
+        return JsonResponse({"success": True, "message": "验证码已发送"})
+
+    @swagger_auto_schema(
+        operation_summary='验证验证码',
+        response={200: 'OK'}
+    )
+    def post(self, request):
+        user_id = request.session.get('_auth_user_id')
+        user = User.objects.get(id=user_id)
+        code = request.POST.get("code")
+        records = EmailVerificationCode.objects.filter(code=code)
+        for record in records:
+            if record.user.id == user.id:
+                user.is_active = True
+                user.save()
+                record.delete()
+                return JsonResponse({"success": True, "message": "验证码正确"})
+        return JsonResponse({"success": False, "message": "验证码错误"})
 
 
-def get_verification_code(request):
-    user_id = request.session.get('_auth_user_id')
-    user = User.objects.get(id=user_id)
-    verification.send_code(user.email)
-    return JsonResponse({"success": True, "message": "验证码已发送"})
+class FindPasswordVerificationCodeView(APIView):
+    @swagger_auto_schema(
+        operation_summary='获取验证码',
+        response={200: 'OK'}
+    )
+    def post(self, request):
+        username = request.POST.get("username")
+        user = User.objects.get(username=username)
+        if not user.is_active:
+            return JsonResponse({"success": False, "message": "该用户未绑定邮箱"})
+        verification.send_code(user.email)
+        return JsonResponse({"success": True, "message": "验证码已发送"})
 
 
-def get_verification_code_with_username(request):
-    username = request.POST.get("username")
-    user = User.objects.get(username=username)
-    if not user.is_active:
-        return JsonResponse({"success": False, "message": "该用户未绑定邮箱"})
-    verification.send_code(user.email)
-    return JsonResponse({"success": True, "message": "验证码已发送"})
-
-
-def verify_code(request):
-    user_id = request.session.get('_auth_user_id')
-    user = User.objects.get(id=user_id)
-    code = request.POST.get("code")
-    records = EmailVerificationCode.objects.filter(code=code)
-    for record in records:
-        if record.user.id == user.id:
-            user.is_active = True
-            user.save()
-            record.delete()
-            return JsonResponse({"success": True, "message": "验证码正确"})
-    return JsonResponse({"success": False, "message": "验证码错误"})
-
-
-def verify_code_and_change_password(request):
-    username = request.POST.get("username")
-    user = User.objects.get(username=username)
-    code = request.POST.get("code")
-    records = EmailVerificationCode.objects.filter(code=code)
-    for record in records:
-        if record.user == user:
-            user.set_password(request.POST.get("password"))
-            user.save()
-            record.delete()
-            return JsonResponse({"success": True, "message": "密码修改成功"})
-    return JsonResponse({"success": False, "message": "验证码错误"})
+class FindPasswordView(APIView):
+    @swagger_auto_schema(
+        operation_summary='找回密码',
+        response={200: 'OK'}
+    )
+    def post(self, request):
+        username = request.POST.get("username")
+        user = User.objects.get(username=username)
+        code = request.POST.get("code")
+        records = EmailVerificationCode.objects.filter(code=code)
+        for record in records:
+            if record.user == user:
+                user.set_password(request.POST.get("password"))
+                user.save()
+                record.delete()
+                return JsonResponse({"success": True, "message": "密码修改成功"})
+        return JsonResponse({"success": False, "message": "验证码错误"})
 
 
 def get_all_posts(request):

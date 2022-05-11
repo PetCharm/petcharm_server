0ea950e446753cbdffaed92238c81a5898e08135
soto <z@mcac.cc>
2022-05-11 14:51:37 +0800

    Application模型调整：新增application_description，db_table更名

--- myapp/models.py
+++ myapp/models.py
@@ -136,5 +136,9 @@ class EmailVerificationCode(models.Model):
 class Application(models.Model):
     application_id = models.AutoField(primary_key=True)
     application_image = models.CharField(max_length=200, blank=True, null=True)
+    application_description = models.CharField(max_length=500, blank=True, null=True)
     application_user = models.ForeignKey(User, models.DO_NOTHING, blank=True, null=True)
     application_date = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'application'

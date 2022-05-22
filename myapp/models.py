3ee2e9476da0d598df271ca59aece88143815a9d
soto <z@mcac.cc>
2022-05-22 18:21:23 +0800

    change TracePath model in field 'trace_path_coordinates'

--- myapp/models.py
+++ myapp/models.py
@@ -148,7 +148,7 @@ class Application(models.Model):
 class TracePath(models.Model):
     trace_path_id = models.AutoField(primary_key=True)
     trace_path_user = models.ForeignKey(User, models.DO_NOTHING, blank=True, null=True)
-    trace_path_coordinates_url = models.CharField(max_length=200, blank=True, null=True)
+    trace_path_coordinates = models.TextField(blank=True, null=True)
     trace_path_start_time = models.DateTimeField(blank=True, null=True)
     trace_path_end_time = models.DateTimeField(blank=True, null=True)
     trace_path_note = models.CharField(max_length=200, blank=True, null=True)

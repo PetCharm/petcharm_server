e6c48ff9eb3cef38ba4792cf496a92b3e8cd1ade
soto <z@mcac.cc>
2022-06-02 18:22:31 +0800

    test AI

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,20 @@
+from django.db.models import *
+
+import os
+
+import django
 from django.test import TestCase
 
 # Create your tests here.
+from myapp import predict
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'back.settings')
+django.setup()
+
+from myapp.models import *
+
+posts = Post.objects.all()
+for post in posts:
+    post.post_senti = predict.senti(post.post_content)
+    post.post_keywords = predict.ner(post.post_content)
+    post.save()

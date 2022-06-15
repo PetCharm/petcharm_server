a167ea3d3ab00a213d3bfb31764fd61ff6470d76
soto <z@mcac.cc>
2022-06-15 22:34:51 +0800

    fix timezone

--- myapp/info.py
+++ myapp/info.py
@@ -10,7 +10,7 @@ def get_pet_info(pet):
         "petType": pet.pet_type,
         "petBreed": pet.pet_breed,
         "petGender": pet.pet_gender,
-        "petAge": (datetime.now(timezone.utc) - pet.pet_date_of_birth).days,
+        "petAge": (datetime.now() - pet.pet_date_of_birth).days,
         "petDateOfBirth": pet.pet_date_of_birth,
         "petVaccinationStatus": pet.pet_vaccination_status,
         "petRegistrationNumber": pet.pet_registration_number,
